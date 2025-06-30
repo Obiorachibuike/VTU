02f82dcf7ff4cfef465f00c9703d1ac52f9d3011
Chibuike Obiora <obiorachibuike22@gmail.com>
2025-06-30 23:00:33 +0100

    Update nofication_page.tsx

--- client/src/app/dashboard/dashboard_component/nofication_page.tsx
+++ client/src/app/dashboard/dashboard_component/nofication_page.tsx
@@ -1,42 +1,45 @@
-import React from 'react'
-import Notification from './notification'
-import '../styles/nofication.css'
+import React from 'react';
+import Notification from './notification';
+import '../styles/nofication.css';
 
 function NoficationPage() {
+  // Optionally, use an array to render multiple notifications
+  const notifications = [
+    {
+      title: "Lorem Ipsum",
+      message: "Quae dolorem earum veritatis oditseno",
+      time: "30 min. ago",
+    },
+    {
+      title: "Lorem Ipsum",
+      message: "Quae dolorem earum veritatis oditseno",
+      time: "30 min. ago",
+    },
+    {
+      title: "Lorem Ipsum",
+      message: "Quae dolorem earum veritatis oditseno",
+      time: "30 min. ago",
+    },
+    {
+      title: "Lorem Ipsum",
+      message: "Quae dolorem earum veritatis oditseno",
+      time: "30 min. ago",
+    },
+  ];
+
   return (
-    <>
-        <div className="note-cont">
-        <div className="note">
-         <Notification
-              title="Lorem Ipsum"
-              message="Quae dolorem earum veritatis oditseno"
-              time="30 min. ago"
-            />
-            </div>
-        <div className="note">
-             <Notification
-              title="Lorem Ipsum"
-              message="Quae dolorem earum veritatis oditseno"
-              time="30 min. ago"
-              />
-              </div>
-              <div className="note">
-             <Notification
-              title="Lorem Ipsum"
-              message="Quae dolorem earum veritatis oditseno"
-              time="30 min. ago"
-              />
-              </div>
-              <div className="note">
-             <Notification
-              title="Lorem Ipsum"
-              message="Quae dolorem earum veritatis oditseno"
-              time="30 min. ago"
-              />
-              </div>
-              </div>
-    </>
-  )
+    <div className="note-cont">
+      {notifications.map((note, index) => (
+        <div className="note" key={index}>
+          <Notification
+            title={note.title}
+            message={note.message}
+            time={note.time}
+          />
+        </div>
+      ))}
+    </div>
+  );
 }
 
-export default NoficationPage
+export default NoficationPage;
