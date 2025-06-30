a184a4c834d77e3391e37962c7dfbf5ad2386301
Chibuike Obiora <obiorachibuike22@gmail.com>
2025-06-30 23:01:33 +0100

    Update notification.tsx

--- client/src/app/dashboard/dashboard_component/notification.tsx
+++ client/src/app/dashboard/dashboard_component/notification.tsx
@@ -1,18 +1,22 @@
-import React from 'react'
+import React from 'react';
 
-function Notification({title,message,time}:any) {
-  return (
-    <>
-     <li className="notification-item">
-              <i className="bi bi-exclamation-circle text-warning"></i>
-              <div>
-                <h4>{title}</h4>
-                <p>{message}</p>
-                <p>{time}</p>
-              </div>
-            </li>
-            </>
-  )
+interface NotificationProps {
+  title: string;
+  message: string;
+  time: string;
 }
 
-export default Notification
+const Notification: React.FC<NotificationProps> = ({ title, message, time }) => {
+  return (
+    <li className="notification-item">
+      <i className="bi bi-exclamation-circle text-warning"></i>
+      <div>
+        <h4>{title}</h4>
+        <p>{message}</p>
+        <p>{time}</p>
+      </div>
+    </li>
+  );
+};
+
+export default Notification;
