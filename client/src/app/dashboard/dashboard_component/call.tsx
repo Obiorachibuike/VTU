fd2cd15c7b6d4106357c0915ad9aa968d65788ff
Chibuike Obiora <obiorachibuike22@gmail.com>
2025-06-30 22:58:41 +0100

    Update call.tsx

--- client/src/app/dashboard/dashboard_component/call.tsx
+++ client/src/app/dashboard/dashboard_component/call.tsx
@@ -1,29 +1,25 @@
-import React from 'react'
-import '../styles/call.css'
+import React from 'react';
+import '../styles/call.css';
 
 function Call() {
   return (
-    <>
-           <div className="col-lg-5 col-12">
-                            <div className=" custom-block-contact call my-4">
-                                <h6 className="mb-4">Still can’t find what you looking for?</h6>
+    <div className="col-lg-5 col-12">
+      <div className="custom-block-contact call my-4">
+        <h6 className="mb-4">Still can’t find what you're looking for?</h6>
 
-                                <p>
-                                    <strong>Call us:</strong>
-                                    <a href="tel: 305-240-9671" className="ms-2">
-                                        (60) 
-                                        305-240-9671
-                                    </a>
-                                </p>
+        <p>
+          <strong>Call us:</strong>
+          <a href="tel:3052409671" className="ms-2">
+            (60) 305-240-9671
+          </a>
+        </p>
 
-                                <a href="#" className="btn custom-btn custom-btn-bg-white mt-3">
-                                    Chat with us
-                                </a>
-                            </div>
-                        </div>
-                    </>
-    // </div>
-  )
+        <a href="#chat" className="btn custom-btn custom-btn-bg-white mt-3">
+          Chat with us
+        </a>
+      </div>
+    </div>
+  );
 }
 
-export default Call
+export default Call;
