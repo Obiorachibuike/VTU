4f57245b9d90f8984127b9c9f84487f81779e5b4
Chibuike Obiora <obiorachibuike22@gmail.com>
2025-07-01 09:04:05 +0100

    Update page.tsx

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,28 +1,30 @@
-// import SEO from "../components/SEO"
+'use client';
+
 import { ThemeProvider } from "./Context/ThemeContext";
 import { UserProvider } from "./Context/UserContext";
 import Main from "./dashboard_component/main";
 import SideNav from "./dashboard_component/side_nav";
 import Layout from "./Layout/Layout";
 import "./styles/airtime.css";
+// import SEO from "../components/SEO"; // Uncomment if SEO component is used
 
 const DashBoard = () => {
   return (
     <>
+      {/* Optional SEO */}
       {/* <SEO
         title="Dashboard Page"
-        description="Recharge your Data,Airtime,Tv Subscription,etc at Cheap,Affordable and Fast Rate"
+        description="Recharge your Data, Airtime, TV Subscription, etc. at Cheap, Affordable, and Fast Rates"
         canonical="http://localhost:3000/dashboard"
       /> */}
 
-      {/* <ThemeProvider> */}
-    
-    <Layout>
-      {/* <UserProvider> */}
-        <Main />
-      {/* </UserProvider> */}
-    </Layout>
-      {/* </ThemeProvider> */}
+      <ThemeProvider>
+        <UserProvider>
+          <Layout>
+            <Main />
+          </Layout>
+        </UserProvider>
+      </ThemeProvider>
     </>
   );
 };
